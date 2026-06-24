3bd82e2ed5eb7f20e1ce2c50ed80c7440cf6e436
Alex Nicolaou <anicolao@gmail.com>
2026-06-24 11:40:07 -0400

    Set up static quilt site

---
+++ README.md
@@ -0,0 +1,19 @@
+# Shadow Box Quilt Designer
+
+Static single-page quilt calculator hosted on GitHub Pages.
+
+## Development
+
+```sh
+nix develop
+npm install
+npm run dev
+```
+
+## Build
+
+```sh
+npm run build
+```
+
+The build copies `quilt.html` to `dist/index.html` for GitHub Pages.
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Tooling for the Shadow Box Quilt Designer SPA";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              git
+              gh
+              nodejs_22
+            ];
+          };
+        });
+    };
+}
